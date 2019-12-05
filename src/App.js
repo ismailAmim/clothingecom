8e0c02b0ba2d76416cbafb9d30a27201f33f8035
bob <ismail.amim2010@gmail.com>
2019-12-05 15:55:59 +0100

    auth doesn t work correctly

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'; 
 import Header   from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.utils';
+import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 
 /* const HatsPage = (props)=> {
   console.log(props);
@@ -63,15 +63,15 @@ class  App extends React.Component {
 
   unsubscribeFromAuth = null;
   componentDidMount (){
-    this.unsubscribeFromAuth=auth.onAuthStateChanged(user=> {
-      
-      this.setState({currentUser:user});
-      console.log(user);
+    this.unsubscribeFromAuth=auth.onAuthStateChanged(async user=> {
+      createUserProfileDocument(user);
+      //this.setState({currentUser:user});
+   // console.log(user);
     })
   }
 
   componentWillUnmount (){
-    this.unsubscribeFromAuth=null;
+    this.unsubscribeFromAuth();
   }
 
  render() {
