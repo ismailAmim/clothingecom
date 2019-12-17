e42118e3b67958b4a58bef318d3db0c4ccc8a6b1
bob <ismail.amim2010@gmail.com>
2019-12-17 11:51:20 +0100

    created cart-item and connected with cartdropdown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import {connect} from "react-redux";
+import CartItem  from '../../components/cart-item/cart-item.component';
 
 import CustomButton from "../custom-button/custom-button.component";
 
 import './cart-dropdown.styles.scss'
 
-const CartDropdown = () => (
+const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
-        <div className="cart-items"/>
+        <div className="cart-items">
+            { cartItems.map(cartitem=><CartItem key={cartitem.id} item={cartitem}/>)
+            // we use cartitem in item object with his props
+            }
+        </div>
         <CustomButton> GO TO CHECKOUT</CustomButton>
     </div>
 )
+const mapStateToProps =({cart:{cartItems}})=> 
+         ({cartItems})
 
-export default CartDropdown;
+export default connect (mapStateToProps)(CartDropdown);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,15 +1,18 @@
 export const addItemToCart = (cartItems,cartItemToAdd)=> {
 
-    const existingCartItem = cartItems. find(
-        cartItem =>cartItem.id ===cartItemToAdd.id);
-
+    const existingCartItem = cartItems.find(cartItem =>cartItem.id ===cartItemToAdd.id);
+    
+    
     if (existingCartItem) {
         return cartItems.map(cartItem=>
             cartItem.id === cartItemToAdd.id 
             ? {...cartItem, quantity : cartItem.quantity +1 }
             : cartItem
              )
+        // map return new array 
+        //if id is match we add new prop quantity to cartItem
+        // else we return the same cartItem
     }
-    //add quantiy 
-    return [...cartItems,{...cartItemToAdd,quantiy : 1}]
+    //if doesn't exist we return cartItems and the cartItem with his quantity
+    return [...cartItems,{...cartItemToAdd,quantity : 1}]
 };
